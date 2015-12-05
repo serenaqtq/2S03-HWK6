bb24b4eecaf4ef37f095b09df211315df5cdf570
dreamingpuffy <pollyyao126@hotmail.com>
2015-12-05 15:53:55 -0500

    Update Addition.cpp

--- Addition.cpp
+++ Addition.cpp
@@ -24,7 +24,7 @@ string Addition::evalute(){
 }
 
 void Addition:: print(){
-	cout << ")" << left << "+" << right << ")" <<output;
+	cout << "(" << left << "+" << right << ")" << "=" <<output;
 }
 
 
